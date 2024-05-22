8d5b6afadb338ffbb8b2e7fd3145aa30a43fed69
Collin Schwantes <schwantes@ecohealthalliance.org>
2024-05-22 16:54:50 -0600

    updated col type for time classes to T

--- R/class_to_col_type.R
+++ R/class_to_col_type.R
@@ -3,6 +3,9 @@
 #' A table that links classes to `readr` column types.
 #' Created from csv file of the same name in inst/
 #'
+#' class_to_col_type <- read.csv(file = "inst/class_to_col_type.csv")
+#' usethis::use_data(class_to_col_type,overwrite = TRUE)
+#'
 #' @format ## `class_to_col_type`
 #' A data frame with 9 rows and 3 columns:
 #' \describe{
